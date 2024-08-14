2c3b54ec1b7ee935fb14ce2589d0f04b66020e38
joseCardona01 <josesiprozmaster@gmail.com>
2024-08-14 01:09:08 -0500

    add component form

--- components/form.tsx
+++
@@ -1,5 +0,0 @@
-export default function Table(){
-    return(
-        <h1>tabla</h1>
-    )
-}
---
+++ components/form/form.tsx
@@ -0,0 +1,30 @@
+export default function Form(){
+    return(
+        <form className="form">
+            <div className="form-title">
+                <h2 className="title">Create product</h2>
+            </div>
+            <fieldset className="form-url">
+                <label className="url-label">Url image:</label>
+                <input className="url-input" type="text"/>
+            </fieldset>
+            <fieldset className="form-title">
+                <label className="title-label">Title:</label>
+                <input className="title-input" type="text"/>
+            </fieldset>
+            <fieldset className="form-description">
+                <label className="description-label">Description:</label>
+                <input className="description-input" type="text"/>
+            </fieldset>
+            <fieldset className="form-description">
+                <label className="description-label">Description:</label>
+                <input className="description-input" type="text"/>
+            </fieldset>
+            <fieldset className="form-price">
+                <label className="price-label">Price:</label>
+                <input className="price-input" type="text"/>
+            </fieldset>
+            <input className="form-button" type="submit" value={"Create"} />
+        </form>
+    )
+}
