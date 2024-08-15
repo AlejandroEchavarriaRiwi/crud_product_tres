8be0104d82afaf33221fc99a6019fecbd05cce55
joseCardona01 <josesiprozmaster@gmail.com>
2024-08-15 01:32:19 -0500

    add method for createProduct and logic for component form

--- utils/util.ts
+++ utils/util.ts
@@ -1,17 +1,17 @@
-import { Product } from "@/interfaces/interfaces";
+import { Product } from "@/type/IProduct";
 
 export default class Util{
     static getProducts(): Product[] | undefined{
-        const products: string | null = localStorage.getItem('products');
-        if(!products){
-            console.log({message: "Error with getProducts"});
-            return;
-        }
-        return JSON.parse(products); // 
+        const products = localStorage.getItem('products') as string;
+        return products ? JSON.parse(products) : []; 
     }
     static createProduct(product: Product): void{
+        console.log(product);
         const getProduct: Product[] | undefined = Util.getProducts();
         getProduct!.push(product);
         localStorage.setItem('products', JSON.stringify(getProduct));
     }
+    static verifyData(...fields: string[]): boolean{
+        return fields.every(field=>field);
+    } 
 }
