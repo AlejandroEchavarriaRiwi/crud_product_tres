631e7f34d40274884e8e731c5d689c9b38387d4d
joseCardona01 <josesiprozmaster@gmail.com>
2024-08-18 15:14:01 -0500

    add logic form register

--- components/ui/form/register/Register.tsx
+++ components/ui/form/register/Register.tsx
@@ -1,6 +1,11 @@
 'use client'
 import Link from "next/link";
 import styled from "styled-components";
+import Input from "../input/inputComponent";
+import { useState } from "react";
+import Button from "../button/buttonComponent";
+import { url } from "inspector";
+import { useRouter } from "next/router";
 
 const primaryColor = "rgba(0, 166, 77, 1)";
 const primaryColorHover = "rgba(0, 166, 77, 0.8)";
@@ -35,33 +40,37 @@ const Label = styled.label`
     color: ${primaryColor};
 `
 
-const Input = styled.input`
-    padding: 10px;
-    border: 1px solid ${primaryColor};
-    border-radius: 4px;
-    font-size: 16px;
+// const Input = styled.input`
+//     padding: 10px;
+//     border: 1px solid ${primaryColor};
+//     border-radius: 4px;
+//     font-size: 16px;
 
-    &:focus {
-        outline: none;
-        box-shadow: 0 0 0 2px ${primaryColorHover};
-    }
-`
+//     &:focus {
+//         outline: none;
+//         box-shadow: 0 0 0 2px ${primaryColorHover};
+//     }
+// `;
 
-const Button = styled.button`
-    padding: 12px;
-    background-color: ${primaryColor};
-    color: white;
-    border: none;
-    border-radius: 4px;
-    cursor: pointer;
-    font-size: 16px;
-    font-weight: bold;
-    transition: background-color 0.3s ease;
+const Fieldset = styled.fieldset`
 
-    &:hover {
-        background-color: ${primaryColorHover};
-    }
-`
+`;
+
+// const Button = styled.button`
+//     padding: 12px;
+//     background-color: ${primaryColor};
+//     color: white;
+//     border: none;
+//     border-radius: 4px;
+//     cursor: pointer;
+//     font-size: 16px;
+//     font-weight: bold;
+//     transition: background-color 0.3s ease;
+
+//     &:hover {
+//         background-color: ${primaryColorHover};
+//     }
+// `
 
 const StyledLink = styled(Link)`
     text-decoration: none;
@@ -77,20 +86,51 @@ const StyledLink = styled(Link)`
 `
 
 const Register: React.FC = () => {
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+    const router = useRouter();
+
+    const handleRegister = async(e:React.FormEvent) =>{
+        e.preventDefault();
+        try{
+            const response = await fetch('http://localhost:3060/api/auth/register',{
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({email,password})
+            });
+            if(response.ok){
+                router.push('/');
+                return;
+            }
+        }catch(error){
+            console.log({message: "Error with the method handleChange", error})
+        }
+    }
     return (
         <ContainerWrapper>
-            <Form>
-                <Label>
-                    Emai
-                    <Input type="email" name="email" required />
-                </Label>
-                <Label>
-                    Password:
-                    <Input type="password" name="password" required />
-                </Label>
-                <Button type="submit">
-                    Registrarse
-                </Button>
+            <Form onSubmit={handleRegister}>
+                <Fieldset>
+                    <Label>Email</Label>
+                    <Input 
+                    type="email" 
+                    name="email"
+                    value={email}
+                    onChange={(e)=>setEmail(e.target.value)} />
+                </Fieldset>
+                <Fieldset>
+                    <Label>Password</Label>
+                    <Input 
+                    type="password" 
+                    name="password"
+                    value={password}
+                    onChange={(e)=>setPassword(e.target.value)} />
+                </Fieldset>
+                <Button 
+                type="submit"
+                value="Send"
+                />
             </Form>
         </ContainerWrapper>
     )
