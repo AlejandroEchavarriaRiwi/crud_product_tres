7bd707efc18f644ed0c3f196672bb5720ddf0e58
Alejandro Echavarria <aechavarriaj@gmail.com>
2024-08-19 13:01:13 -0500

    addstyles

--- components/ui/footer/MainFooter.tsx
+++ components/ui/footer/MainFooter.tsx
@@ -12,32 +12,28 @@ const MainFooter:React.FC<INavbar>=({page})=> {
         </div>
         <div className='FooterNavCenter'>
             <div>
-                <h1><Link href="/">Explore</Link></h1>
-                <h3><Link href="/">Explore</Link></h3>
-                <h3><Link href="/">Explore</Link></h3>
-                <h3><Link href="/">Explore</Link></h3>
+                <h1>Explore</h1>
+                <h3><Link href="/">Home</Link></h3>
+                <h3><Link href="/">About us</Link></h3>
             </div>
             <div>
-                <h1><Link href="/">Explore</Link></h1>
-                <h3><Link href="/">Explore</Link></h3>
-                <h3><Link href="/">Explore</Link></h3>
-                <h3><Link href="/">Explore</Link></h3>
+                <h1>Visit</h1>
+                <h3>Guayabal</h3>
+                <h3>Cl. 16 #55-129</h3>
             </div>
             <div>
-                <h1><Link href="/">Explore</Link></h1>
-                <h3><Link href="/">Explore</Link></h3>
-                <h3><Link href="/">Explore</Link></h3>
-                <h3><Link href="/">Explore</Link></h3>
+                <h1>Follow us</h1>
+                <h3><Link href="http://www.instagram.com">Instagram</Link></h3>
+                <h3><Link href="http://www.facebook.com">Explore</Link></h3>
             </div>
             <div>
-                <h1><Link href="/">Explore</Link></h1>
-                <h3><Link href="/">Explore</Link></h3>
-                <h3><Link href="/">Explore</Link></h3>
-                <h3><Link href="/">Explore</Link></h3>
+                <h1>Legal</h1>
+                <h3><Link href="/">Term</Link></h3>
+                <h3><Link href="/">Privacy</Link></h3>
             </div>
         </div>
         <div className='FooterNavRight'>
-            <div className='WhiteCircle w-8 h-8'></div>
+            <div className='WhiteCircle w-8 h-8'><div className='triangle'></div></div>
         </div>
     </div>
 
